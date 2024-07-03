aa61ec05abac55317d1e4491fc387fec9899aef0
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2024-07-03 23:19:26 +0600

    add project

--- src/Components/ProjectDetails.jsx
+++ src/Components/ProjectDetails.jsx
@@ -1,137 +1,20 @@
-import "./ProjectDetails.css";
 import { useEffect, useState } from "react";
+import "./ProjectDetails.css";
 import { useParams } from "react-router-dom";
 
 const ProjectDetails = () => {
   const { id } = useParams();
-  const [project, setProject] = useState({});
-
-  const Projcets = [
-    {
-      id: 1,
-      title: "Flex Code",
-      team: "Team Project",
-      name: "Problem solving Website",
-      description: [
-        "Flex-Code is a problem-solving website. Where users can practice coding problems.",
-        "Providing a playground, where users can create projects by using HTML, CSS, and JavaScript.",
-        "A solver can challenge another solver against a problem. Whoever solves it first win.",
-        "user can see user profile. who can top and solve how it's problem.",
-      ],
-      image: "https://i.ibb.co/jwqJPJq/flex-code-6541d-web-app.png",
-      links: {
-        live: "https://flex-code-6541d.web.app/",
-        client: "https://github.com/ihshadin/flex-code",
-        server: "https://github.com/ihshadin/flex-code-server",
-      },
-      technology: [
-        "JavaScript",
-        "React",
-        "Tailwind",
-        "ExpresJs",
-        "Mongoose",
-        "Firebase",
-        "sslcommerz",
-        "jwt",
-        "vercel",
-      ],
-      screenshots: [
-        "https://i.ibb.co/3hYWVp5/Screenshot-2023-10-14-192822.png",
-        "https://i.ibb.co/LdRDPXh/Screenshot-2023-10-14-193022.png",
-        "https://i.ibb.co/n3Q8ncf/Screenshot-2023-10-14-193109.png",
-        "https://i.ibb.co/vQH1Qxr/Screenshot-2023-10-14-200733.png",
-      ],
-    },
-    {
-      id: 2,
-      title: "CAR WORLD",
-      name: "Car Company Web App",
-      description: [
-        "Browse and search for different toy car models.",
-        "View detailed information and specifications of each car.",
-        "Filter cars based on criteria such as brand, price etc.",
-        "Save favorite cars for future reference.",
-        "User authentication and account management",
-      ],
-      image: "https://i.ibb.co/hBc1n3j/toy-car-ce3f5-web-app-1.png",
-      links: {
-        live: "https://toy-car-ce3f5.web.app/",
-        client: "https://github.com/mehedihasan8/Toy-car-world",
-        server: "https://github.com/mehedihasan8/Toy-car-world-server",
-      },
-      technology: [
-        "Html",
-        "Css",
-        "JavaScript",
-        "React",
-        "Tailwind",
-        "DaisyU",
-        "ExpresJs",
-        "Firebase",
-        "MongoDB",
-      ],
-      screenshots: [
-        "https://i.ibb.co/jW01v8n/Screenshot-2023-06-28-052158.png",
-        "https://i.ibb.co/fpkHgHZ/Screenshot-2023-06-28-052308.png",
-        "https://i.ibb.co/pdSdvrb/Screenshot-2023-06-28-052342.png",
-        "https://i.ibb.co/M5FtfYd/Screenshot-2023-06-28-052445.png",
-      ],
-    },
-    {
-      id: 3,
-      title: "YOGA",
-      name: "Yoga Classes And Yoga Instrucors site",
-      description: [
-        "Browse and search for different Classes.",
-        "View detailed information and specifications of each yoga class.",
-        "payment based on his class price base etc.",
-        "post instracturs class and see enrol details",
-        "User authentication and account management and save user data",
-      ],
-      image:
-        "https://i.ibb.co/w63L1QS/simple-firebase-project-a5441-web-app.png",
-      links: {
-        live: "https://simple-firebase-project-a5441.web.app/",
-        client: "https://github.com/mehedihasan8/Yoga-client-site",
-        server: "https://github.com/mehedihasan8/Yoga-server",
-      },
-      technology: [
-        "Html",
-        "Css",
-        "JavaScript",
-        "React",
-        "Tailwind",
-        "DaisyU",
-        "ExpresJs",
-        "MongoDB",
-        "Json Webtoken",
-        "Stripe",
-      ],
-      screenshots: [
-        "https://i.ibb.co/MM25cd2/Screenshot-2023-06-30-213539.png",
-        "https://i.ibb.co/bQtqbqz/Screenshot-2023-06-30-213932.png",
-        "https://i.ibb.co/m9SL4kL/Screenshot-2023-06-30-214100.png",
-        "https://i.ibb.co/hZCbYFV/Screenshot-2023-06-30-214344.png",
-      ],
-    },
-  ];
-
+  const [singleProjects, setSingleProjects] = useState();
   useEffect(() => {
-    const project = Projcets.find((x) => x.id === +id);
-    setProject(project);
-  }, []);
-
-  const {
-    image,
-    title,
-    name,
-    description,
-    technology,
-    links,
-    screenshots,
-    team,
-  } = project;
-
+    fetch(`http://localhost:5000/api/projects/${id}`)
+      .then((res) => res.json())
+      .then((data) => setSingleProjects(data?.data));
+  }, [id]);
+  const sentences = singleProjects?.description
+    .split(".")
+    .map((sentence) => sentence.trim())
+    .filter((sentence) => sentence.length > 0);
+  console.log(singleProjects);
   return (
     <div
       id="projectditails"
@@ -140,27 +23,27 @@ const ProjectDetails = () => {
       <div
         data-aos="zoom-in-right"
         data-aos-duration="1500"
-        className="inside-detail  max-w-screen-lg mx-auto flex flex-col text-white lg:flex-row justify-center items-start gap-8 lg:h-[60vh] text-left mb-20"
+        className="inside-detail max-w-screen-lg mx-auto flex flex-col text-white lg:flex-row justify-center items-start gap-8 text-left mb-20"
       >
         <div className="image-detail w-full h-[35rem] lg:w-1/2 lg:h-full rounded-xl overflow-hidden">
-          <img src={image} alt="" />
+          <img src={singleProjects?.image} alt="" />
         </div>
         <div className="w-full lg:w-1/2 flex flex-col  justify-center">
           <div>
             <div className="flex justify-evenly items-center">
               <h2 className="font-semibold text-base  text-secondary">
-                {title}
+                {singleProjects?.title}
               </h2>
               <h1 className="font-bold text-2xl  text-[#0097e6] ">
-                [ {team} ]
+                {singleProjects?.team}
               </h1>
             </div>
             <h2 className="project-title text-accent font-semibold text-xl my-4">
-              {name}
+              {singleProjects?.name}
             </h2>
             <div className="my-4">
               <h3 className=" text-secondary">Project Overview</h3>
-              {description?.map((point, index) => (
+              {sentences?.map((point, index) => (
                 <p key={index} className="text-accent">
                   {index + 1}. {point}
                 </p>
@@ -169,7 +52,7 @@ const ProjectDetails = () => {
             <div className="my-5">
               <h3 className=" text-secondary">Technology used</h3>
               <div className="flex flex-wrap gap-2">
-                {technology?.map((point, index) => (
+                {singleProjects?.tech?.map((point, index) => (
                   <button
                     key={index}
                     data-aos="zoom-in"
@@ -188,7 +71,7 @@ const ProjectDetails = () => {
             className="button-content flex justify-between mt-5"
           >
             <a
-              href={links?.live}
+              href={singleProjects?.liveLink}
               target="_blank"
               rel="noreferrer"
               className="project-button text-[10px] lg:text-[12px]"
@@ -197,7 +80,7 @@ const ProjectDetails = () => {
               View Live
             </a>
             <a
-              href={links?.client}
+              href={singleProjects?.githubClient}
               target="_blank"
               rel="noreferrer"
               className="project-button text-[10px] lg:text-[12px]"
@@ -206,7 +89,7 @@ const ProjectDetails = () => {
               Client Code
             </a>
             <a
-              href={links?.server}
+              href={singleProjects?.githubServer}
               target="_blank"
               rel="noreferrer"
               className="project-button text-[10px] lg:text-[12px]"
@@ -219,7 +102,7 @@ const ProjectDetails = () => {
         </div>
       </div>
 
-      <div className=" max-w-screen-lg px-8 md:px-0 mb-20">
+      {/* <div className=" max-w-screen-lg px-8 md:px-0 mb-20">
         <p className="text-white text-center font-bold text-xl lg:text-5xl mb-16 ">
           Project Screenshots
         </p>
@@ -235,7 +118,7 @@ const ProjectDetails = () => {
             />
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/Components/Protfolio.jsx
+++ src/Components/Protfolio.jsx
@@ -1,46 +1,51 @@
-import car_toy_img from "../assets/toy-car-ce3f5.web.app_ (1).png";
-import flex_code_img from "../assets/flex-code-6541d.web.app_.png";
-import yoga_img from "../assets/simple-firebase-project-a5441.web.app_.png";
+// import car_toy_img from "../assets/toy-car-ce3f5.web.app_ (1).png";
+// import flex_code_img from "../assets/flex-code-6541d.web.app_.png";
+// import yoga_img from "../assets/simple-firebase-project-a5441.web.app_.png";
 import "./Protfoliol.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Protfolio = () => {
+  const [projects, setProjects] = useState([]);
   useEffect(() => {
+    fetch("http://localhost:5000/api/projects")
+      .then((res) => res.json())
+      .then((data) => setProjects(data?.data));
     AOS.init({ duration: 1000 });
   }, []);
-  const portfolios = [
-    {
-      id: 1,
-      src: flex_code_img,
-      name: "Flex Code",
-      scroll: "fade-down",
-      liveLink: "https://flex-code-6541d.web.app/",
-      code: "https://github.com/mehedihasan8/Food-lover-client",
-    },
-    {
-      id: 2,
-      src: car_toy_img,
-      name: "CAR YOY",
-      scroll: "fade-up",
-      liveLink: "https://toy-car-ce3f5.web.app/",
-      code: "https://github.com/mehedihasan8/Toy-car-world",
-    },
-    {
-      id: 3,
-      src: yoga_img,
-      name: "YOGA",
-      scroll: "fade-down",
-      liveLink: "https://simple-firebase-project-a5441.web.app/",
-      code: "https://github.com/mehedihasan8",
-    },
-  ];
+  console.log(projects);
+  // const portfolios = [
+  //   {
+  //     id: 1,
+  //     src: flex_code_img,
+  //     name: "Flex Code",
+  //     scroll: "fade-down",
+  //     liveLink: "https://flex-code-6541d.web.app/",
+  //     code: "https://github.com/mehedihasan8/Food-lover-client",
+  //   },
+  //   {
+  //     id: 2,
+  //     src: car_toy_img,
+  //     name: "CAR YOY",
+  //     scroll: "fade-up",
+  //     liveLink: "https://toy-car-ce3f5.web.app/",
+  //     code: "https://github.com/mehedihasan8/Toy-car-world",
+  //   },
+  //   {
+  //     id: 3,
+  //     src: yoga_img,
+  //     name: "YOGA",
+  //     scroll: "fade-down",
+  //     liveLink: "https://simple-firebase-project-a5441.web.app/",
+  //     code: "https://github.com/mehedihasan8",
+  //   },
+  // ];
   return (
     <div
       id="portfolio"
-      className=" bg-gradient-to-b from-black to-gray-800 w-full   md:pt-0 text-white "
+      className="bg-gradient-to-b from-black to-gray-800 w-full   md:pt-0 text-white "
     >
       <div className="max-w-screen-lg p-4 md:p-0 md:pt-16 mx-auto flex flex-col justify-center w-full ">
         <p className="text-4xl font-bold inline  border-b-4 border-gray-500">
@@ -53,35 +58,35 @@ const Protfolio = () => {
           data-aos-duration="1500"
           className="portfolio-car grid sm:grid-cols-2 md:grid-cols-3 gap-7 px-4 sm:px-0 md:mt-3"
         >
-          {portfolios.map(({ id, src, liveLink, name, scroll }) => (
+          {projects.map((project) => (
             <div
               data-aos={scroll}
               data-aos-easing="linear"
               data-aos-duration="1500"
-              key={id}
+              key={project?._id}
               className="shadow-md shadow-sky-500 rounded-lg border-2 border-sky-500 overflow-hidden"
             >
               <div className="image-content  overflow-hidden">
                 <img
-                  src={src}
+                  src={project?.image}
                   alt=""
                   className="px-3 py-3 mt-2 duration-200 hover:scale-105"
                 />
               </div>
               <p className="text-2xl text-bold pb-2 text-gray-400 ml-3">
-                {name}
+                {project?.name}
               </p>
               <div className="flex items-center justify-center pb-4">
                 <a
                   className="w-1/2 rounded border-2 text-center bg-cyan-500  border-sky-500 px-3 py-1 m-4 duration-200 hover:scale-105"
-                  href={liveLink}
+                  href={project?.liveLink}
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <button> VIEW LIVE</button>
+                  <button>VIEW LIVE</button>
                 </a>
                 <div className="w-1/2">
-                  <Link to={`/project/${id}`} smoth>
+                  <Link to={`/project/${project?._id}`} smoth>
                     <button className=" px-2 border-2   rounded text-cyan-500 border-sky-500 py-1 duration-200 hover:scale-105 ">
                       VIEW DETAILS
                     </button>
